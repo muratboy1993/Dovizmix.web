7f69a8b9b96ef3746338900c181cd7aec76be2d3
muratboy1993 <73284946+muratboy1993@users.noreply.github.com>
2020-10-23 13:00:26 +0300

    Add files via upload

---
+++ Infrastructure.Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Middleware
+{
+
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        //private readonly ILoggerManager _logger;
+
+        //public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        //{
+        //    _logger = logger;
+        //    _next = next;
+        //}
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            dynamic json = new 
+            {
+                data = "",
+                status = false,
+                message = exception.Message,
+
+            };
+
+
+
+            //convert json to a stream
+            var buffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(json, Formatting.Indented));
+            await context.Response.Body.WriteAsync(buffer);
+
+        
+        }
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ Infrastructure.Middleware/LoginAndLastViewPageDateUpdateMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Infrastructure.ServiceManager.Abstract;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Infrastructure.Middleware
+{
+    public class LoginAndLastViewPageDateUpdateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IUserManager _userManager;
+
+        public LoginAndLastViewPageDateUpdateMiddleware(RequestDelegate next,IUserManager userManager)
+        {
+            _userManager = userManager;
+            _next = next;
+            
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                //todo: serkan burası düşünülecek sadece bu sayfada kullanıcı loign date update yapılıyor
+                if (httpContext.User.Identity.IsAuthenticated&&!httpContext.Request.Path.Value.Contains("assets"))
+                {
+                    int userId = Convert.ToInt32(httpContext.User.Claims.FirstOrDefault(c => c.Type == "UserId").Value);
+
+                    //todo: Burada sayfa adına ulaşmak gerekiyor.
+                    await _userManager.LoginAndLastPageDateUpdate(userId, httpContext.Request.Path);
+                   
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                
+                //throw new Exception(ex.Message);
+            }
+            finally
+            {
+                await _next.Invoke(httpContext);
+            }
+
+          
+        }
+    }
+
+    public static class LoginAndLastViewPageDateUpdateMiddlewareExtension
+    {
+        public static IApplicationBuilder LoginAndLastViewPageDateUpdateMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoginAndLastViewPageDateUpdateMiddleware>();
+        }
+    }
+}
