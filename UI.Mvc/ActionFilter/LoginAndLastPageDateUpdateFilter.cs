21764de336f35b5ade17346b094fba4b962e6dbb
muratboy1993 <73284946+muratboy1993@users.noreply.github.com>
2020-10-23 13:16:34 +0300

    Add files via upload

---
+++ UI.Mvc/ActionFilter/LoginAndLastPageDateUpdateFilter.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.ServiceManager.Abstract;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Mvc.ActionFilter
+{
+    public class LoginAndLastPageDateUpdateFilter : IActionFilter
+    {
+        private readonly IUserManager _userManager;
+        
+
+        public LoginAndLastPageDateUpdateFilter(IUserManager userManager)
+        {
+            _userManager = userManager;
+        
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var route = context.RouteData.Values["action"];
+                _userManager.LoginAndLastPageDateUpdate(Convert.ToInt32(context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "UserId").Value), route.ToString());
+
+            }
+           
+           
+        }
+    }
+}
